c5caf6852623f6d6f3921f2fcb3cdb1e7ea4a703
TNJKL <sinhngay21042004@gmail.com>
2025-06-16 17:13:01 +0700

    Add project files.

---
+++ WebSiteBanMoHinh/Migrations/20250519143117_tihi.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebSiteBanMoHinh.Migrations
+{
+    /// <inheritdoc />
+    public partial class tihi : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FullName",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FullName",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Orders");
+        }
+    }
+}
