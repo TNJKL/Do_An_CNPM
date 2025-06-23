20c3b0b7b64e1e1c287f3f802cc78867c38b2ed0
TNJKL <sinhngay21042004@gmail.com>
2025-06-23 17:16:41 +0700

    FG-161 #done: Thiết kế giao diện lịch sử đơn hàng

--- WebSiteBanMoHinh/Areas/Admin/Repository/IEmailSender.cs
+++ WebSiteBanMoHinh/Areas/Admin/Repository/IEmailSender.cs
@@ -1,1 +1,8 @@
-﻿
+﻿namespace WebSiteBanMoHinh.Areas.Admin.Repository
+{
+    public interface IEmailSender
+    {
+        //Task SendEmailAsync(string email, string subject, string message);
+        Task SendEmailAsync(string email, string subject, string message, bool isHtml = false);
+    }
+}
