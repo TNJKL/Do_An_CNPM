4dbbe579e2b0c655967e17bf5b076b505b6aef90
KhanhPhan <khanhphan.040204@gmail.com>
2025-06-24 14:27:56 +0700

    FG-148 #done:   Tạo giao diện form quên mật khẩu

--- WebSiteBanMoHinh/Helpers/ICurrentUserService.cs
+++ WebSiteBanMoHinh/Helpers/ICurrentUserService.cs
@@ -1,1 +1,10 @@
-﻿
+﻿using WebSiteBanMoHinh.Models;
+
+namespace WebSiteBanMoHinh.Helpers
+{
+    public interface ICurrentUserService
+    {
+        string UserId { get; }
+        Task<AppUserModel> GetUser();
+    }
+}
