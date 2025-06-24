5576efc57f88c4ee35565c7cd3b81174627cb166
KhanhPhan <khanhphan.040204@gmail.com>
2025-06-24 14:53:48 +0700

    FG-214 #done: Admin quản lý đơn hàng

--- WebSiteBanMoHinh/Services/Vnpay/IVnPayService.cs
+++ WebSiteBanMoHinh/Services/Vnpay/IVnPayService.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using WebSiteBanMoHinh.Models.Vnpay;
+
+namespace WebSiteBanMoHinh.Services.Vnpay
+{
+    public interface IVnPayService
+    {
+        string CreatePaymentUrl(PaymentInformationModel model, HttpContext context);
+        PaymentResponseModel PaymentExecute(IQueryCollection collections);
+
+    }
+}
