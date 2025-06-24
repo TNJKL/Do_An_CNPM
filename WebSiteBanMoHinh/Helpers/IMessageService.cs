71ea9d7ef66db998a1b7749e5cf6366e4f21fe3c
KhanhPhan <khanhphan.040204@gmail.com>
2025-06-24 14:28:51 +0700

    FG-149 #done:   Kiểm tra hợp lệ định dạng email đầu vào

--- WebSiteBanMoHinh/Helpers/IMessageService.cs
+++ WebSiteBanMoHinh/Helpers/IMessageService.cs
@@ -1,1 +1,10 @@
-﻿
+﻿using WebSiteBanMoHinh.ViewModels;
+
+namespace WebSiteBanMoHinh.Helpers
+{
+    public interface IMessageService
+    {
+        Task<IEnumerable<MessagesUsersListViewModel>> GetUsers();
+        Task<ChatViewModel> GetMessages(string selectedUserId);
+    }
+}
